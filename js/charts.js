4b137f487c1fc352c44a110e39f5a921a468c7a8
Kyle <kylekarpack@gmail.com>
2013-11-30 12:53:26 -0800

    Last changes for the quarter

--- js/charts.js
+++ js/charts.js
@@ -2,12 +2,15 @@ var timeData = [],
 	mouseDataCount = [],
 	clicksCount = [],
 	keypressesCount = [],
+	pagesCount = [],
 	speeds = [],
+	
 	categories = [],
 	chart;
 
 $(window).load(function() {
 	
+
 	// UI Stuff
 	$("#log").click(function() {
 		chart.yAxis[0].isLog = !(chart.yAxis[0].isLog);
@@ -26,33 +29,42 @@ $(window).load(function() {
 				keypresses = 0,
 				networkSpeed = 0;	
 			
-			var pages = 0;
-			for (var j in i) {
+			var pages = 0,
+				pageTitlesSet = new Set();
+			for (var j in i) {					
 				
-				var goodData = typeof(t[i][j]) == 'undefined';
+				var goodData = typeof(t[i][j]) != 'undefined',
+					time,
+					mouse,
+					k,
+					c,
+					n;
 				
-				var time = goodData ? 0 : t[i][j]["timeOnPage"];
-				var mouse = goodData ? 0 : t[i][j]["mouseposition"].length;
-				var k = goodData ? 0 : (typeof(t[i][j]["keypress"]) == 'undefined' ? 0 : t[i][j]["keypress"].length);
-				var c = goodData ? 0 : (typeof(t[i][j]["click"]) == 'undefined' ? 0 : t[i][j]["click"].length);
-				var n = goodData ? 0 : parseFloat(t[i][j]["networkSpeed"]);
-			
-				taskTime += time;
-				mouseMovements += mouse;
-				keypresses += k;
-				clicks += c;
-				console.log(c);
-				
-				if (!isNaN(n)) {
-					networkSpeed += n;
-					pages++;
+				if (goodData) {
+					time =  t[i][j]["timeOnPage"];
+					mouse = t[i][j]["mouseposition"].length;
+					k = (typeof(t[i][j]["keypress"]) == 'undefined' ? 0 : t[i][j]["keypress"].length);
+					c = (typeof(t[i][j]["click"]) == 'undefined' ? 0 : t[i][j]["click"].length);
+					n =  parseFloat(t[i][j]["networkSpeed"]);
+					pageTitlesSet.add(t[i][j]["title"]);
+					
+					taskTime += time;
+					mouseMovements += mouse;
+					keypresses += k;
+					clicks += c;
+					
+					if (!isNaN(n)) {
+						networkSpeed += n;
+						pages++;
+					}
 				}
 			}
 			categories.push(i);
-			timeData[count] = taskTime / 1000;
+			timeData[count] = Math.round(taskTime / 1000);
 			mouseDataCount[count] = mouseMovements;
 			keypressesCount[count] = keypresses;
 			clicksCount[count] = clicks;
+			pagesCount[count] = pageTitlesSet.size;
 			speeds[count] = Math.round(networkSpeed / pages);
 			
 			count++;
@@ -62,9 +74,19 @@ $(window).load(function() {
 		chart.series[1].setData(mouseDataCount);
 		chart.series[2].setData(clicksCount);
 		chart.series[3].setData(keypressesCount);
-		chart.series[4].setData(speeds);
+		chart.series[4].setData(pagesCount);
+		chart.series[5].setData(speeds);
 	});
 
+	Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function(color) {
+		return {
+			radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
+			stops: [
+				[0, color],
+				[1, Highcharts.Color(color).brighten(-0.2).get('rgb')] // darken
+			]
+		};
+	});
 	
      chart = $('#chart').highcharts({
             chart: {
@@ -99,7 +121,7 @@ $(window).load(function() {
                 enabled: false
             },
             series: [{
-				name: 'Time on Site',
+				name: 'Time on Task',
 				data: []
 			},{
 				name: 'Mouse Movements',
@@ -109,6 +131,9 @@ $(window).load(function() {
 				data: []
 			},{
 				name: 'Keypresses',
+				data: []
+			},{
+				name: 'Pages Visited',
 				data: []
 			},{
 				name: 'Free Network Speed',
